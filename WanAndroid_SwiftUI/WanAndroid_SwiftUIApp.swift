992d41d9bd960c873ad2c161e60d7cdc663bdae7
user <zxc_developer@126.com>
2023-06-05 18:07:21 +0800

    Initial Commit

---
+++ WanAndroid_SwiftUI/WanAndroid_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanAndroid_SwiftUIApp.swift
+//  WanAndroid_SwiftUI
+//
+//  Created by zxc on 2023/6/5.
+//
+
+import SwiftUI
+
+@main
+struct WanAndroid_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
